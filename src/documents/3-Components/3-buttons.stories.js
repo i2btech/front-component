1cc591671409564f4aa5afa9db16d817445af3f1
Wesley Altamiranda <wesley.altamiranda@i2btech.com>
2020-06-06 21:22:18 -0500

    StoryBook button update

--- src/documents/3-Components/3-buttons.stories.js
+++ src/documents/3-Components/3-buttons.stories.js
@@ -4,7 +4,14 @@ export const Button = () => `
 	<div class="p-30">
 		<h2 class="text fz-28">Botones</h2>
 
-		<div class="mt-20 text-white">
+		<p class="mt-20">Incluye varios estilos de botones predefinidos, cada uno con su propio propósito semántico, con algunos extras para mayor control.</p>
+
+
+		<h3 class="text fz-24 mt-30">Colores</h3>
+
+		<p class="mt-20">Agregado <b>bg-default</b>, <b>bg-primary</b>, <b>bg-secondary</b>, <b>bg-danger</b>, <b>bg-black</b>, <b>disabled</b></p>
+
+		<div class="mt-15 text-white">
 			<button class="btn bg-default">Defecto</button>
 			<button class="btn bg-primary ml-5">Primario</button>
 			<button class="btn bg-secondary ml-5">Secundario</button>
@@ -13,7 +20,11 @@ export const Button = () => `
 			<button class="btn bg-black ml-5" disabled="">Deshabilitado</button>
 		</div>
 
-		<div class="mt-10 text-white">
+		<h3 class="text fz-24 mt-30">Tamaños</h3>
+
+		<p class="mt-20">Agregado <b>btn--sm</b>, <b>btn--md</b>, <b>btn--lg</b>, <b>btn--xl</b></p>
+
+		<div class="mt-15 text-white">
 			<button class="btn bg-black btn--sm">Pequeño</button>
 			<button class="btn bg-black ml-5">Defecto</button>
 			<button class="btn bg-black btn--md ml-5">Medio</button>
