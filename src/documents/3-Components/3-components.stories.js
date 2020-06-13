d0aa925480bccd66a284f8db5f9f6186cf140ea7
Wesley Altamiranda <wesley.altamiranda@i2btech.com>
2020-06-13 17:24:01 -0500

    Storybook Form add

--- src/documents/3-Components/3-components.stories.js
+++ src/documents/3-Components/3-components.stories.js
@@ -15,3 +15,14 @@ Form.story = { name: 'Formulario' }
   button.innerText = '😀 😎 👍 💯';
   return button;
 };*/
+
+/*var script = document.createElement('script');
+script.src = '../../../dev/assets/js/libs/libs.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+$(function () {
+	console.log('Select', $js('csel'));
+	// Form Select
+	$js('csel').i2bSelect();
+});*/
